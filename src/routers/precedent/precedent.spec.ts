4f231e4d112c7d680aa7ee9e79545d7d2535a526
Max Kim <hwaseen@gmail.com>
2020-08-07 00:27:43 +0900

    fix:complete text format

--- src/routers/precedent/precedent.spec.ts
+++ src/routers/precedent/precedent.spec.ts
@@ -1,61 +1,70 @@
 import { Response } from 'supertest'
+import { Express } from 'express'
 import { Precedent } from '../../@types/supreme-court-api/precedent.d'
-import { mockGetResponse } from '../../utils/testHelper'
+import { mockGetResponse, loadApp } from '../../utils/testHelper'
 
 describe('GET /precedent', () => {
+  let app:Express
+  let res:Response
+  beforeAll(async () => {
+    app = await loadApp()
+  })
   describe('/precedent', () => {
-    let res:Response
-    describe('요청 성공시', async () => {
-      res = await mockGetResponse('/precedent')
+    describe('요청 성공시', () => {
       it('상태코드 200을 반환한다.', async (done) => {
+        res = await mockGetResponse(app, '/precedent')
         expect(res.status).toBe(200)
         done()
       })
       it('precedent[]를 반환한다', async (done) => {
-        expect(res.body).toBe(200)
+        res = await mockGetResponse(app, '/precedent')
+        expect(typeof res.body).toBe('object')
         done()
       })
     })
   })
 
   describe('/precedent?type=', () => {
-    let res:Response
-    describe('요청 성공시', async () => {
-      res = await mockGetResponse('/precedent/?type=criminal')
+    describe('요청 성공시', () => {
       it('상태코드 200을 반환한다.', async (done) => {
+        res = await mockGetResponse(app, '/precedent?=criminal')
         expect(res.status).toBe(200)
         done()
       })
-      it('상태코드 200을 반환한다.', async (done) => {
-        expect(res.status).toBe(200)
+      it('type에 맞는 precedent만 가져온다.', async (done) => {
+        res = await mockGetResponse(app, '/precedent?=criminal')
+        res.body.forEach((elem:Precedent) => {
+          expect(elem.type).toBe('criminal')
+        })
         done()
       })
     })
-    describe('요청 실패시', async () => {
-      res = await mockGetResponse('/precedent/?type=general')
+    describe('요청 실패시', () => {
       it('type이 criminal이나 civil이 아닐 경우 409를 뱉는다', async (done) => {
-        expect(res.status).toBe(200)
+        res = await mockGetResponse(app, '/precedent?=general')
+        expect(res.status).toBe(409)
         done()
       })
     })
   })
 
   describe('/precedent?page=', () => {
-    let res:Response
-    describe('요청 성공시', async () => {
-      res = await mockGetResponse('/precedent/?page=2')
+    describe('요청 성공시', () => {
       it('상태코드 200을 반환한다.', async (done) => {
+        res = await mockGetResponse(app, '/precedent/?page=2')
         expect(res.status).toBe(200)
         done()
       })
       it('5개의 precedent로 이루어진 precedent[]를 반환한다.', async (done) => {
+        res = await mockGetResponse(app, '/precedent/?page=2')
         expect(res.body.length).toBe(5)
+        expect(typeof res.body).toBe('Array')
         done()
       })
     })
-    describe('요청 실패시', async () => {
-      res = await mockGetResponse('/precedent/?page=some')
+    describe('요청 실패시', () => {
       it('page가 정수가 아닐 경우 409를 반환한다.', async (done) => {
+        res = await mockGetResponse(app, '/precedent/?page=some')
         expect(res.status).toBe(409)
         done()
       })
--- src/utils/testHelper.ts
+++ src/utils/testHelper.ts
@@ -2,20 +2,18 @@ import request, { Response } from 'supertest'
 import express, { Express } from 'express'
 import loaders from '../loaders'
 
-const loadApp = async ():Promise<Express> => {
+export const loadApp = async ():Promise<Express> => {
   const app:Express = express()
   await loaders(app)
   return app
 }
 
-export const mockGetResponse = async (url:string):Promise<Response> => {
-  const app:Express = await loadApp()
+export const mockGetResponse = async (app:Express, url:string):Promise<Response> => {
   const res:Response = await request(app).get(url)
   return res
 }
 
-export const mockPostResponse = async (url:string):Promise<Response> => {
-  const app:Express = await loadApp()
+export const mockPostResponse = async (app:Express, url:string):Promise<Response> => {
   const res:Response = await request(app).post(url)
   return res
 }
